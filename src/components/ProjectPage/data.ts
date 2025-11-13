a6b102e1cd78cde153c176763acd27c1cfa8f845
Obliviate <756541536@qq.com>
2025-11-13 14:29:15 +0800

    feat: update services, article, & alumni (#23)

--- src/components/ProjectPage/data.ts
+++ src/components/ProjectPage/data.ts
@@ -31,7 +31,7 @@ const keyValuePublications = keyValuePubsId.map((id) => {
 const graphPubsId: number[] = [
   0,1,6,7,8,9,11,12,13,14,17,18,19,20,
   22,24,25,27,29,31,32,34,35,36,38,40,42,43,
-  44,45,47,48,49,50,53,54,56,61,62,64,65,66,67
+  44,45,47,48,49,50,53,54,56,59,61,62,64,65,66,67
 ];
 
 const graphPublications = graphPubsId.map((id) => {
