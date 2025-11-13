fe39783c2d02bd831131ad7cd53b5a6bbf2de7e7
buchuitoudegou <756541536@qq.com>
2025-11-13 14:24:57 +0800

    update services, article, & alumni

--- src/components/Publication/pub.ts
+++ src/components/Publication/pub.ts
@@ -47,6 +47,24 @@ export const pubData: PubType[] = pubDatabase.sort((a, b) => {
   if (a.conference !== "PVLDB" && b.conference === "PVLDB") {
     return 1;
   }
+  if (a.conference === "ICDE" && b.conference !== "ICDE") {
+    return -1;
+  }
+  if (a.conference !== "ICDE" && b.conference === "ICDE") {
+    return 1;
+  }
+  if (a.conference === "ICML" && b.conference !== "ICML") {
+    return -1;
+  }
+  if (a.conference !== "ICML" && b.conference === "ICML") {
+    return 1;
+  }
+  if (a.conference === "NeurIPS" && b.conference !== "NeurIPS") {
+    return -1;
+  }
+  if (a.conference !== "NeurIPS" && b.conference === "NeurIPS") {
+    return 1;
+  }
   if (a.conference === "IsJournal" && b.conference !== "IsJournal") {
     return -1;
   }
