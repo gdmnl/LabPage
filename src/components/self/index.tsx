80090fe384df8e4c272a0df25f5a5d82f0d80345
lostrong <luosiqiang912@126.com>
2026-08-21 20:09:56 +0800

    simplify homepage (#54)
    Co-authored-by: Luo Siqiang <luosiqiang@DMAL-LuoSQY25NB1.local>

--- src/components/self/index.tsx
+++ src/components/self/index.tsx
@@ -3,7 +3,6 @@ import * as React from "react";
 import { Col, Row } from "antd";
 import { Content } from "antd/es/layout/layout";
 import NewsList from "./news";
-import FundingList from "./fundingList";
 
 export class SelfIntro extends React.Component {
   public render()  {
@@ -84,9 +83,6 @@ export class SelfIntro extends React.Component {
         </div>
         <div style={{ display: 'flex', justifyContent: 'center' }}>
           <NewsList />
-        </div>
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <FundingList />
         </div>
       </Content>
     );
